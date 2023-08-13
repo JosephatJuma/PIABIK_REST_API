29fb1ceb6bcd0b9ea040bddade95169fbea60b15
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-08-13 13:27:47 +0300

    add api routes

--- app.js
+++ app.js
@@ -2,3 +2,19 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("App is running");
+});
+//import main router
+const main = require("./routes/main");
+app.use("/v1/api", main);
+
+app.listen(port, () => {
+  console.log(`Piabik Rest API is running on port ${port}`);
+});
---
+++ database/database.js
@@ -0,0 +1,16 @@
+const dotenv = require("dotenv");
+const mysql = require("mysql2"); //npm i mysql2
+
+dotenv.config();
+const database = mysql
+  .createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DATABASEPORT,
+    connectTimeout: 60000, // Set connection timeout to 60 seconds
+  })
+  .promise();
+
+module.exports = database;
