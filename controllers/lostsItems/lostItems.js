0ea09fa930f7128adefb55b2979e868fadbdca2b
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-08-15 23:26:24 +0300

    fix lost item post end point

--- controllers/lostsItems/lostItems.js
+++ controllers/lostsItems/lostItems.js
@@ -1,17 +1,37 @@
 const db = require("../../database/database");
 exports.postLostItem = async (req, res) => {
-  const { category, name, description, location, contact, uniqueId } = req.body;
+  const type = "Lost";
+  const {
+    uniqueId,
+    category,
+    speciality,
+    phone,
+    firstName,
+    lastName,
+    status,
+    datePosted,
+    secretCode,
+  } = req.body;
   try {
-    const sql = `INSERT INTO lostItems (category, name, description, location, contact, uniqueId) VALUES (?, ?,?,?, ?, ?) ORDER BY DatePosted ASC`;
-    await db.query(sql, [
-      category,
-      name,
-      description,
-      location,
-      contact,
-      uniqueId,
-    ]);
-    res.send("Lost items successfully posted");
+    const sql = `INSERT INTO lost_items ( UniqueID,Category,Type,Speciality,Phone,FirstName,LastName,Status,DatePosted,SecretCode) VALUES (?, ?,?,?, ?, ?,?,?,?,?)`;
+    const connection = await db();
+    await connection
+      .query(sql, [
+        uniqueId,
+        category,
+        type,
+        speciality,
+        phone,
+        firstName,
+        lastName,
+        status,
+        datePosted,
+        secretCode,
+      ])
+      .then((result) => {
+        connection.release();
+        res.send({ message: "Lost item successfully posted", status: true });
+      });
   } catch (error) {
     res.status(500).send(error.message);
   }
